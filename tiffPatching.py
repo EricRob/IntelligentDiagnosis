d714fd554553ac90b6829c0fadca234d39a8f008
Eric <eric@metriccollective.com>
2017-11-29 18:36:18 -0500

    Fixed resizing and x_index reset

--- tiffPatching.py
+++ tiffPatching.py
@@ -8,8 +8,7 @@ from scipy import misc
 from matplotlib.image import imread
 from numpy.lib.stride_tricks import as_strided
 from math import floor
-from skimage.measure import block_reduce
-from skimage.io import imsave
+from PIL import Image
 
 
 # Create patches and load into 6-dimension array with properties:
@@ -51,7 +50,7 @@ def extract_patches (originalPath, patchSize, overlap):
 
 # Create a folder to hold patches if one doesn't exist, then
 # use it to save tiffs from the patches array
-def save_patches (originalPath, patches, patchSize, overlap, sampleFactor):
+def save_patches (originalPath, patches, patchSize, overlap, sampleSize):
 
     # Create enclosing folder to hold patch dump
     folderPath = os.path.abspath(os.path.join(originalPath, os.pardir)) + '/' + os.path.splitext(originalPath)[0] + '_patches'
@@ -64,10 +63,15 @@ def save_patches (originalPath, patches, patchSize, overlap, sampleFactor):
     print('Saving...')
     for i in range(patches.shape[0]):
         for j in range(patches.shape[1]):
+            # name is [orignal title]_[top left x pixel]_[top left y pixel]
             name = folderPath + '/' +  os.path.splitext(originalPath)[0] + '_' + str(x_index) + '_' + str(y_index) + '.tif'
-            downsample = block_reduce(patches[i,j,0,:,:,:], block_size=(sampleFactor,sampleFactor,1), func=np.mean)
-            imsave(name,downsample)
+
+            img = Image.fromarray(patches[i,j,0,:,:,:], 'RGB')
+            img = img.resize((sampleSize,sampleSize), Image.ANTIALIAS)
+            img.save(name)
+
             x_index += floor(patchSize*(1-overlap*.01))
+        x_index = 0
         y_index += floor(patchSize*(1-overlap*.01))
 
     return patches.shape[0]*patches.shape[1]
@@ -80,12 +84,12 @@ if __name__ == '__main__':
     originalPath = str(sys.argv[1])
     patchSize = int(sys.argv[2])
     overlap = int(sys.argv[3])
-    sampleFactor = int(sys.argv[4])
+    sampleSize = int(sys.argv[4])
 
 
     patches = extract_patches(originalPath, patchSize, overlap)
 
-    numPatches = save_patches(originalPath, patches, patchSize, overlap, sampleFactor)
+    numPatches = save_patches(originalPath, patches, patchSize, overlap, sampleSize)
 
     print('Created ' + str(numPatches) + ' patches from ' + originalPath)
 
