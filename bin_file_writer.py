5765912df188a074cc2817ab7d9b5df810f4d63d
Eric <eric@metriccollective.com>
2017-12-27 19:03:18 -0500

    Create bin_file_writer
    Needs to be run individually for recurrence and non-recurrence

---
+++ bin_file_writer.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+import sys
+from skimage import io
+
+# ATTN: must be run within parent folder for set of patches
+
+if __name__ == '__main__':
+
+	# pass in binary file name as first parameter
+	bin_name = str(sys.argv[1])
+
+	# create a new byte file that can be appended
+	bin_file = open(bin_name, "ab+")
+
+	# walk over all files in starting directory and sub-directories
+	for dirpath, dirnames, filenames in os.walk("."):
+		lst = os.listdir(dirpath)
+		print(dirpath)
+		lst.sort()
+		for filename in [f for f in lst if f.endswith(".tif")]:
+			# open tiff as grayscale, convert to unsiged 8-bit integer, and append to file
+			img = io.imread(os.path.join(dirpath,filename), as_grey=True)
+			arr = np.floor(img*255)
+			arr = np.uint8(arr)
+			bin_file.write(arr.tobytes())
+
+	bin_file.close()
+
+	sys.exit(0)
