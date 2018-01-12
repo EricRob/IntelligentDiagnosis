120c1a67c8c20eebd284291e919ed6a1ead8216e
Eric <eric@metriccollective.com>
2018-01-12 12:30:59 -0500

    Add logic for data augmentation option
    New required parameters of sequence length (num_steps) and sequence overlap

--- bin_file_writer.py
+++ bin_file_writer.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import sys
 from skimage import io
+from IPython import embed
+import math
 
 # ATTN: must be run within parent folder for set of patches
 
@@ -9,6 +11,8 @@ if __name__ == '__main__':
 
 	# pass in binary file name as first parameter
 	bin_name = str(sys.argv[1])
+	sequence_overlap_percentage = int(sys.argv[2]) / 100
+	num_steps = int(sys.argv[3])
 
 	# create a new byte file that can be appended
 	bin_train_file = open(str(bin_name + "_train.bin"), "ab+")
@@ -19,30 +23,50 @@ if __name__ == '__main__':
 	VALID_PORTION = 0.2
 	TEST_PORTION = 0.1
 
+	IMAGE_BYTES = 10000
+
+	write_array = np.zeros([num_steps, IMAGE_BYTES], dtype=np.uint8)
+	write_stride = int(math.ceil(num_steps * (1-sequence_overlap_percentage)))
+
 	total_files = sum([len(files) for r, d, files in os.walk(".")])
-	counter = 1
+	counter = 0
 
 	# walk over all files in starting directory and sub-directories
 	for dirpath, dirnames, filenames in os.walk("."):
 		lst = os.listdir(dirpath)
-		print(dirpath)
-		lst.sort() # need files in order by index (see fix_patch_alpha) so time series is accurately fed to RNN
+		lst.sort() # need files in order by index (see fix_patch_alpha) so time series is accurately fed to RNN]
+		
 		for filename in [f for f in lst if f.endswith(".tif")]:
+			
+			counter +=1
+
 			# open tiff as grayscale, convert to unsiged 8-bit integer, and append to file
 			img = io.imread(os.path.join(dirpath,filename), as_grey=True)
 			arr = np.floor(img*255)
 			arr = np.uint8(arr)
-			if(counter / total_files < TRAIN_PORTION):
-				bin_train_file.write(arr.tobytes())
-				counter += 1
-			elif(counter / total_files < (TRAIN_PORTION + VALID_PORTION)):
-				bin_valid_file.write(arr.tobytes())
-				counter += 1
-			else:
-				bin_test_file.write(arr.tobytes())
+			arr = arr.flatten()
+
+			# At start of iterations, first fill write_array with data until it contains one complete sequence
+			if(counter <= num_steps):
+				write_array[counter-1,:] = arr
+			else: # Shift all images in the write_array to the left one step, then replace the last value with the new image data
+				write_array = np.roll(write_array, -1, axis=0)
+				write_array[num_steps - 1,:] = arr
+
+			# After the a new portion of the sequence has been added, add the write_array to the binary file
+			if(counter % write_stride == 0):
+				print(filename + " -- writing " + str(counter))
+				if(counter / total_files < TRAIN_PORTION):
+					bin_train_file.write(write_array.tobytes())
+				elif(counter / total_files < (TRAIN_PORTION + VALID_PORTION)):
+					bin_valid_file.write(write_array.tobytes())
+				else:
+					bin_test_file.write(write_array.tobytes())
 
 	bin_train_file.close()
 	bin_valid_file.close()
 	bin_test_file.close()
+
+	print("stride: " + str(write_stride))
 
 sys.exit(0)
