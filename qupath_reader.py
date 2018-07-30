b5f5b36b1ca6f5d6ef9fc8efb987d03c8a8c76f8
EricRob <eric.james.robinson@gmail.com>
2018-07-30 14:22:10 -0400

    Initial reading of detections data

--- qupath_reader.py
+++ qupath_reader.py
@@ -11,19 +11,31 @@ import numpy as np
 import csv
 import os
 import sys
+from IPython import embed
+
 
 # Global variables
+DETECTIONS = '/data/QuPath/CellCounter/detections'
 
 # Class declarations
 
 # Function declarations
 
 def main():
-    args = sys.argv[1:]
+	d = {}
+	n = 1
+	tsvfile = '00_02_1_1 Detectionstxt.txt'
+	tsvfile = os.path.join(DETECTIONS, tsvfile)
+	with open(tsvfile, 'r') as f:
+		reader = csv.DictReader(f, delimiter='\t')
+		print(reader.fieldnames)
+		for row in reader:
+			d['{0:06}'.format(n)] = row
+			n += 1
+	for cell in d:
+		d[cell].pop('Centroid Y px', None)
+		d[cell].pop('Centroid X px', None)
 
-    if not args:
-        print('usage: [--flags options] [inputs] ')
-        sys.exit(1)
 
 # Main body
 if __name__ == '__main__':
