db2a131e3c6d33ae3d588a56c38a99bc78069a2b
Eric Robinson <eric.james.robinson@gmail.com>
2019-04-09 08:58:25 -0400

    Limit checkpoint fixing to the model being run and add a hack helper for when --omen is forgotten

--- batch_testing.py
+++ batch_testing.py
@@ -26,7 +26,8 @@ parser.add_argument('--omen', default=False, action='store_true', help='Run on O
 parser.add_argument('--fullyc', default=False, action='store_true', help='Run fully connected models only')
 
 
-CU_MODELS = ['FC_CU_Apr_05','CU_Feb_11_200', 'CU_Sinai_Feb_23_200']
+CU_MODELS = ['CU_Apr_05', 'FC_CU_Apr_05','CU_Feb_11_200', 'CU_Sinai_Feb_23_200']
+CU_MODELS = ['CU_Feb_11_200', 'CU_Sinai_Feb_23_200']
 YALE_MODELS = ['Yale_Feb_15_200']
 
 
@@ -34,7 +35,8 @@ ARGS = parser.parse_args()
 
 
 class PrecisionConfig(object):
-	RESULTS_DIR = '/data/recurrence_seq_lstm/results/'
+	RESULTS_DIR = os.path.join('/data', 'recurrence_seq_lstm', 'results')
+	REPLACE_DIR = os.path.join('/hdd', 'ID_net', 'results')
 	DATA_CONDITIONS = '/data/recurrence_seq_lstm/data_conditions/'
 	SCRIPT_DIR = '/home/wanglab/Desktop/recurrence_seq_lstm/IntelligentDiagnosis/'
 	YALE_TEST_DIR = os.path.join('/data', 'recurrence_seq_lstm', 'data_conditions', 'yale_testing_data')
@@ -46,6 +48,7 @@ class PrecisionConfig(object):
 
 class OmenConfig(object):
 	RESULTS_DIR = os.path.join('/hdd', 'ID_net', 'results')
+	REPLACE_DIR = os.path.join('/data', 'recurrence_seq_lstm', 'results')
 	DATA_CONDITIONS = os.path.join('/hdd', 'ID_net', 'data_conditions')
 	SCRIPT_DIR = '/hdd/ID_net/IntelligentDiagnosis/'
 	YALE_TEST_DIR = os.path.join('/hdd', 'ID_net', 'data_conditions', 'yale_testing_data')
@@ -81,18 +84,62 @@ def test_model(base, model, test_dir, name, config):
 
 	return
 
-def fix_checkpoints():
+def fix_checkpoints(config):
 	# TBD - Loop over all available models and change checkpoint path to config's path.
+	for root, dirs, files in os.walk(config.RESULTS_DIR):
+		for d in dirs:
+			replacing = False
+			checkpoint_file = os.path.join(root, d, 'checkpoint')
+			writing = []
+			if os.path.exists(checkpoint_file):
+				with open(checkpoint_file, 'r') as f:
+					for line in f:
+						if config.RESULTS_DIR in line:
+							continue
+						else:
+							replacing = True
+							writing.append(line.replace(config.REPLACE_DIR, config.RESULTS_DIR))
+				if replacing:
+					cprint('Replacing checkpoints in ' + d)
+					with open(checkpoint_file, 'w+') as f:
+						for line in writing:
+							f.write(line)
+
+							
+		return
 	return
+
+def fix_checkpoint(model, config):
+	checkpoint_file = os.path.join(config.RESULTS_DIR, model, 'checkpoint')
+	writing = []
+	if os.path.exists(checkpoint_file):
+		print('Verifying checkpoints for ' + model + '...')
+		with open(checkpoint_file, 'r') as f:
+			for line in f:
+				if config.REPLACE_DIR in line:
+					writing.append(line.replace(config.REPLACE_DIR, config.RESULTS_DIR))
+			if writing:
+				cprint('Replacing checkpoints in ' + d)
+				with open(checkpoint_file, 'w+') as f:
+					for line in writing:
+						f.write(line)
+		return 0
+	else:
+		return 1
 
 def main():
 
-	fix_checkpoints()
+	tower_name = os.uname().nodename
+	if 'OMEN' in tower_name:
+		ARGS.omen = True
 
 	if ARGS.omen:
 		config = OmenConfig()
 	else:
 		config = PrecisionConfig()
+	
+	# fix_checkpoints(config)
+
 	fc_base = 'python3 ' + os.path.join(config.SCRIPT_DIR, 'recurrence_lstm_FC_features.py') + ' --config=test ' + config.OMEN
 	dnn_base = 'python3 ' + os.path.join(config.SCRIPT_DIR, 'recurrence_lstm_features.py') + ' --config=test ' + config.OMEN
 	models = []
@@ -107,6 +154,11 @@ def main():
 		models = fc_models
 
 	for model in models:
+		exit = fix_checkpoint(model, config)
+		if exit:
+			cprint('No valid checkpoint file for ' + model)
+			continue
+
 		if 'FC' in model:
 			base = fc_base
 		else:
