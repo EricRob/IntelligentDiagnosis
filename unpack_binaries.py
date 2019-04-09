ad06e2c5986675382e1ec452e4905f526777e638
Eric Robinson <eric.james.robinson@gmail.com>
2019-04-09 13:57:08 -0400

    Fixed byte counts

--- unpack_binaries.py
+++ unpack_binaries.py
@@ -14,31 +14,31 @@ parser = argparse.ArgumentParser(description='Unpack binary files to determine t
 
 # Global variables
 parser.add_argument('--cond', default=None, required=True, type=str, help='Name of condition to unpack')
+parser.add_argument('--mode', default='train', type=str, help='Name of mode to unpack')
 
 SEQ_LENGTH = 20
 NUM_FEATURES = 7
 
 
 SUBJ_BYTES = 5
-NAME_BYTES = 92
+NAME_BYTES = 99
 COORD_BYTES = SEQ_LENGTH*2*6
 FEAT_BYTES = NUM_FEATURES*8
 IMG_BYTES = SEQ_LENGTH*100*100*3
-
-DATA_CONDITIONS = os.path.join('/data', 'recurrence_seq_lstm', 'data_conditions')
+RECORD_BYTES = SUBJ_BYTES + NAME_BYTES + COORD_BYTES + FEAT_BYTES + IMG_BYTES
 
 
 ARGS = parser.parse_args()
 # Class declaration
 
 def main():
+    DATA_CONDITIONS = os.path.join('/data', 'recurrence_seq_lstm', 'data_conditions')
     tower_name = os.uname().nodename
     if 'OMEN' in tower_name:
         DATA_CONDITIONS = os.path.join('/hdd', 'ID_net', 'data_conditions')
 
-    BINARIES = ['recurrence_train.bin', 'recurrence_test.bin', 'recurrence_valid.bin',
-    'nonrecurrence_train.bin', 'nonrecurrence_test.bin', 'nonrecurrence_valid.bin']
-
+    BINARIES = ['recurrence_' + ARGS.mode + '.bin', 'nonrecurrence_' + ARGS.mode + '.bin']
+    data = {}
 
     for binary in BINARIES:
             with open(os.path.join(DATA_CONDITIONS, ARGS.cond, binary), 'rb') as file:
@@ -51,7 +51,13 @@ def main():
                     if name not in data[subj]:
                         data[subj].append(name)
                         print('subject: ' + subj + ' -- image: ' + name)
-    pdb.set_trace()
+    with open(os.path.join(DATA_CONDITIONS, ARGS.cond, ARGS.cond + '_' + ARGS.mode + '_contents.csv'), 'w+') as file:
+        writer = csv.writer(file)
+        writer.writerow(['subject', 'image'])
+        for key, value in data.items():
+            for v in value:
+                writer.writerow([key, v])
+    cprint('Summary saved to ' + os.path.join(DATA_CONDITIONS, ARGS.cond, ARGS.cond + '_' + ARGS.mode + '_contents.csv'), 'cyan')
 
 # Main body
 if __name__ == '__main__':
